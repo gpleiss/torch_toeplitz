5a259908b4a4316acad68138062a7c66a0968cfa
Geoff Pleiss <gpleiss@gmail.com>
2017-04-22 23:25:51 -0400

    Add some docs

--- torchtoeplitz/util.py
+++ torchtoeplitz/util.py
@@ -1,9 +1,26 @@
 import torch
 
+'''
+    Reverses a tensor
+    Args:
+        - input: tensor to reverse
+        - dim: dimension to reverse on
+    Returns:
+        - reversed input
+'''
 def reverse(input, dim=0):
     reverse_index = torch.LongTensor(list(range(input.size(dim))[::-1]))
     return input.index_select(dim, reverse_index)
 
+
+'''
+    Computes a reverse cumulative sum
+    Args:
+        - input: tensor
+        - dim: dimension to reverse on
+    Returns:
+        - rcumsum on input
+'''
 def rcumsum(input, dim=0):
     reverse_index = torch.LongTensor(list(range(input.size(dim))[::-1]))
     return torch.index_select(input, dim, reverse_index).cumsum(dim).index_select(dim, reverse_index)
